0d3bb01ad0dbb8aa6864f545cb24e1ed4ba58b1f
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-08-11 19:41:46 +0530

    Parse functionality for different types of values

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const SelectFields = require('./lib/select-fields');
-const JoinFields = require('./lib/join-fields');
+const DefaultValue = require('./lib/default-value');
 const InsertFields = require('./lib/insert-fields');
+const JoinFields = require('./lib/join-fields');
+const ModelField = require('./lib/model-field');
+const Parser = require('./lib/parse');
 const PrivateFields = require('./lib/private-fields');
 const QueryValues = require('./lib/query-values');
+const SelectFields = require('./lib/select-fields');
 const UpdateValues = require('./lib/update-values');
-const DefaultValue = require('./lib/default-value');
 const ValuesPointer = require('./lib/values-pointer');
-const ModelField = require('./lib/model-field');
 
 
 module.exports = {
-  SelectFields,
-  JoinFields,
+  DefaultValue,
   InsertFields,
+  JoinFields,
+  ModelField,
+  Parser,
   PrivateFields,
   QueryValues,
+  SelectFields,
   UpdateValues,
-  DefaultValue,
-  ValuesPointer,
-  ModelField
+  ValuesPointer
 };
