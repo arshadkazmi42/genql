d3a2b7f6ac6d78ba53f20f07f8777b63261bfbc9
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-08-21 22:52:47 +0530

    Restructure complete code, removed parse functionalities, created class functions

--- index.js
+++ index.js
@@ -1,24 +1,122 @@
-const DefaultValue = require('./lib/default-value');
-const InsertFields = require('./lib/insert-fields');
-const JoinFields = require('./lib/join-fields');
-const ModelField = require('./lib/model-field');
-const Parser = require('./lib/parse');
-const PrivateFields = require('./lib/private-fields');
-const QueryValues = require('./lib/query-values');
-const SelectFields = require('./lib/select-fields');
-const UpdateValues = require('./lib/update-values');
-const ValuesPointer = require('./lib/values-pointer');
+const _ = require('lodash');
 
 
-module.exports = {
-  DefaultValue,
-  InsertFields,
-  JoinFields,
-  ModelField,
-  Parser,
-  PrivateFields,
-  QueryValues,
-  SelectFields,
-  UpdateValues,
-  ValuesPointer
+function GenQL(model) {
+
+  if (!model) {
+    throw new Error('Model is required');
+  }
+
+  if (!model.columns) {
+    throw new Error('Invalid model structure');
+  }
+
+  this.model = model;
+}
+
+
+GenQL.prototype._default = function(field) {
+  if (!field) {
+    throw new Error('Field is required');
+  }
+  
+  const column = this._find(field);
+  if (!column) {
+    return;
+  }
+
+  return column.defaultValue;
 };
+
+
+GenQL.prototype._find = function(field) {
+  if (!field) {
+    throw new Error('Field is required');
+  }
+
+  return _.find(this.model.columns, { name: field });
+};
+
+
+GenQL.prototype._insert = function(prefix) {
+
+  prefix = prefix ? `${prefix}.` : '';
+  
+  const columns = _.filter(this.model.columns, (column) => !column.auto);
+  return _.map(columns, column => {
+    return `${prefix}${column.name}`;
+  });
+};
+
+
+GenQL.prototype._join = function(prefix) {
+
+  prefix = prefix ? `${prefix}.` : '';
+  const { join_key, columns } = this.model;
+  
+  return _.map(columns, (column) => {
+    if (column.alternate) {
+      return `COALESCE(NULLIF(${prefix}${column.name}, ''), ${column.alternate}) as ${join_key}_${column.name}`;
+    }
+
+    return `${prefix}${column.name} as ${join_key}_${column.name}`;
+  });
+};
+
+
+GenQL.prototype._pointer = function(length) {
+  if (!length) {
+    throw new Error('Length is required');
+  }
+
+  return _.times(length, _.constant('?'));
+};
+
+
+GenQL.prototype._findPrivate = function(prefix) {
+
+  prefix = prefix ? `${prefix}.` : '';
+  
+  const columns = _.filter(this.model.columns, column => column.private);
+  return _.map(columns, (column) => {
+    return `${prefix}${column.name}`;
+  });
+};
+
+
+GenQL.prototype._removePrivate = function(data) {
+
+  return _.omit(data, this._findPrivate());
+};
+
+
+GenQL.prototype._values = function(data) {
+
+  const columns = _.filter(this.model.columns, column => !column.auto);
+  return _.map(columns, column => {
+    return _.get(data, column.name) || column.defaultValue;
+  });
+};
+
+
+GenQL.prototype._select = function(prefix) {
+  prefix = prefix ? `${prefix}.` : '';
+
+  return _.map(this.model.columns, column => {
+    if (column.alternate) {
+      return `COALESCE(NULLIF(${prefix}${column.name}, ''), ${column.alternate})`;
+    }
+
+    return `${prefix}${column.name}`;
+  });
+};
+
+
+GenQL.prototype._update = function(fields, data) {
+  return _.map(fields, field => {
+    return _.get(data, field) || this._default(field);
+  });
+};
+
+
+module.exports = GenQL;
--- tests/test.js
+++ tests/test.js
@@ -1,202 +1,118 @@
 const { expect } = require('chai');
-const _ = require('lodash');
 
-const { 
-  DefaultValue,
-  InsertFields,
-  JoinFields,
-  PrivateFields,
-  Parser,
-  QueryValues,
-  SelectFields,
-  UpdateValues,
-  ValuesPointer
-} = require('../index');
+const GenQL = require('../index');
 
-const BLOB_DATA = '{"type":"Buffer","data":[118,49,48,215,237,167,249,243,19,0,154,172,45,177,18,115,66,18,133,179,117,163,148,13,106,231,219,121,53,252,127,128,5,50,52,161,101,76,166,202,30,253,119,76,22,61,251,177,167,134,247,60,43,193,42,129,125,11,161,21,164,70,169,83,153,213,84,90,234,33,155,190,35,132,106,194,85,196,50,107,105,9,69,44,21,81,101,39,7,192,226,244,206,44,135,11,108,137,190,249,20,133,81,242,184,81,105,249,46,153,103,235,63,53,237,104,61,19,101,36,15,185,191,124,48,176,112,220,218,180,41,144,163,227,127,157,93,124,228,63,246,58,111,171,57,64,245]}';
-const FIELDS = ['name', 'class', 'status'];
 const MODEL = require('../data/model.json');
+const $ = new GenQL(MODEL);
+
+const FIELDS = ['name', 'email', 'class', 'status'];
+const INSERT_FIELDS = ['$.name', '$.email', '$.class', '$.status'];
+const POINTERS = ['?', '?', '?'];
+const DATA_NO_AUTO = ['Arshad Kazmi', 'arshadkazmi42@gmail.com', '1st', 'active'];
 const DATA = {
-  'name': {
-    json: 'Arshad'
-  },
-  'email': 'arshadkazmi42@gmail.com',
-  'class': 'none'
+  id: '1',
+  name: 'Arshad Kazmi',
+  email: 'arshadkazmi42@gmail.com',
+  class: '1st',
+  status: 'active'
 };
 
 const DATA_NO_PRIVATE = {
-  'name': {
-    json: 'Arshad'
-  },
-  'class': 'none'
+  id: '1',
+  name: 'Arshad Kazmi',
+  class: '1st',
+  status: 'active'
+};
+
+const DEFAULT_VALUE = {
+  name: 'status',
+  defaultValue: 'active'
 };
 
 const SELECT_FIELDS = [
+  '$.id',
+  '$.name',
+  '$.email',
+  'COALESCE(NULLIF($.class, \'\'), cls)',
+  '$.status'
+];
+
+const SELECT_FIELDS_WITHOUT_PREFIX = [
   'id',
   'name',
   'email',
-  'class',
+  'COALESCE(NULLIF(class, \'\'), cls)',
   'status'
 ];
 
-const INSERT_FIELDS = [
-  'prefix.name',
-  'prefix.email',
-  'prefix.class',
-  'prefix.status'
+const JOIN_SELECT_FIELDS = [
+  '$.id as students_id',
+  '$.name as students_name',
+  '$.email as students_email',
+  'COALESCE(NULLIF($.class, \'\'), cls) as students_class',
+  '$.status as students_status'
 ];
 
-const JOIN_FIELDS = [
+const JOIN_SELECT_FIELDS_WO_PREFIX = [
   'id as students_id',
   'name as students_name',
   'email as students_email',
-  'class as students_class',
+  'COALESCE(NULLIF(class, \'\'), cls) as students_class',
   'status as students_status'
 ];
 
-const QUERY_VALUES = [
-  {
-    json: 'Arshad'
-  },
-  'arshadkazmi42@gmail.com',
-  'none',
-  'active'
-];
 
-const UPDATE_VALUES = [
-  {
-    json: 'Arshad'
-  },
-  'none',
-  'active'
-];
-
-
-describe('test private-fields functions', () => {
-  it('should remove private fields from data', () => {
-    try  {
-      PrivateFields.remove();
-    } catch (err) {
-      expect(err.message).to.equal('Model and data are required');
-    }
-
-    const fields = PrivateFields.remove(MODEL, _.cloneDeep(DATA));
-    expect(fields).to.deep.equal(DATA_NO_PRIVATE);
+describe('genql functionality validation', () => {
+  it('should create genql object', () => {
+    expect($.model).to.deep.equal(MODEL);
   });
-  it('should return the private fields', () => {
-    try  {
-      PrivateFields.get();
-    } catch (err) {
-      expect(err.message).to.equal('Model is required');
-    }
-
-    const fields = PrivateFields.get(MODEL);
-    expect(fields).to.deep.equal(['email']);
+  it('should return default value of the field', () => {
+    expect($._default(DEFAULT_VALUE['name'])).to.equal(DEFAULT_VALUE['defaultValue']);
   });
-});
-
-describe('Util functionality', () => {
-  it('should return list of pointers', () => {
+  it('should return the column from model', () => {
+    expect($._find(DEFAULT_VALUE['name'])).to.deep.equal(DEFAULT_VALUE);
+  });
+  it('should return list of insert columns with prefix', () => {
+    expect($._insert('$')).to.deep.equal(INSERT_FIELDS);
+  });
+  it('should return list of insert columns without prefix', () => {
+    expect($._insert()).to.deep.equal(FIELDS);
+  });
+  it('should return list of select fields with join with preifx', () => {
+    expect($._join('$')).to.deep.equal(JOIN_SELECT_FIELDS);
+  });
+  it('should return list of select fields with join without prefix', () => {
+    expect($._join()).to.deep.equal(JOIN_SELECT_FIELDS_WO_PREFIX);
+  });
+  it('should throw error invalid length', () => {
     try {
-      ValuesPointer.get();
+      $._pointer();
     } catch (err) {
       expect(err.message).to.equal('Length is required');
     }
-
-    const pointers = ValuesPointer.get(5);
-    expect(pointers.length).to.equal(5);
-    expect(pointers).to.deep.equal(['?', '?', '?', '?', '?']);
   });
-});
-
-describe('test default values functionality', () => {
-  it('should return default value if available', () => {
-    try  {
-      DefaultValue.get();
-    } catch (err) {
-      expect(err.message).to.equal('Model and field name are required');
-    }
-
-    let defaultValue = DefaultValue.get(MODEL, 'status');
-    expect(defaultValue).to.equal('active');
-
-    defaultValue = DefaultValue.get(MODEL, 'name');
-    expect(defaultValue).to.equal(undefined);
+  it('should return pointers array', () => {
+    expect($._pointer(3)).to.deep.equal(POINTERS);
   });
-});
-
-describe('test insert fields functionalities', () => {
-  it('should return all insert fields', () => {
-    try  {
-      InsertFields.get();
-    } catch (err) {
-      expect(err.message).to.equal('Model is required');
-    }
-
-    const fields = InsertFields.get(MODEL, 'prefix');
-    expect(fields).to.deep.equal(INSERT_FIELDS);
+  it('should return private fields without preifx', () => {
+    expect($._findPrivate()).to.deep.equal(['email']);
   });
-});
-
-describe('test join fields functionalities', () => {
-  it('should return all join fields', () => {
-    try  {
-      JoinFields.get();
-    } catch (err) {
-      expect(err.message).to.equal('Model is required');
-    }
-
-    const fields = JoinFields.get(MODEL);
-    expect(fields).to.deep.equal(JOIN_FIELDS);
+  it('should return private fields with preifx', () => {
+    expect($._findPrivate('$')).to.deep.equal(['$.email']);
   });
-});
-
-describe('test query fields functionalities', () => {
-  it('should return the query field values', () => {
-    try  {
-      QueryValues.get();
-    } catch (err) {
-      expect(err.message).to.equal('Model and data are required');
-    }
-
-    const values = QueryValues.get(MODEL, DATA);
-    expect(values).to.deep.equal(QUERY_VALUES);
+  it('should return data without private fields', () => {
+    expect($._removePrivate(DATA)).to.deep.equal(DATA_NO_PRIVATE);
   });
-});
-
-describe('test select fields functionalities', () => {
-  it('should return all select columns from model', () => {
-    try  {
-      SelectFields.get();
-    } catch (err) {
-      expect(err.message).to.equal('Model is required');
-    }
-
-    const fields = SelectFields.get(MODEL);
-    expect(fields).to.deep.equal(SELECT_FIELDS);
+  it('should return query fields', () => {
+    expect($._values(DATA)).to.deep.equal(DATA_NO_AUTO);
   });
-});
-
-describe('test all update values', () => {
-  it('should return all update values', () => {
-    try  {
-      UpdateValues.get();
-    } catch (err) {
-      expect(err.message).to.equal('Model, fields and data are required');
-    }
-
-    const values = UpdateValues.get(MODEL, FIELDS, DATA);
-    expect(values).to.deep.equal(UPDATE_VALUES);
+  it('should return select fields without prefix', () => {
+    expect($._select()).to.deep.equal(SELECT_FIELDS_WITHOUT_PREFIX);
   });
-});
-
-describe('test parser', () => {
-  it('should return parsed buffer value', () => {
-    const blob = Parser.get('blob', BLOB_DATA);
-    expect(blob).to.deep.equal(Buffer.from(JSON.parse(BLOB_DATA), 'utf8'));
+  it('should return select fields with prefix', () => {
+    expect($._select('$')).to.deep.equal(SELECT_FIELDS);
   });
-  it('should return parsed json value', () => {
-    const blob = Parser.get('json', BLOB_DATA);
-    expect(blob).to.deep.equal(JSON.parse(BLOB_DATA), 'utf8');
+  it('should return update field values', () => {
+    expect($._update(FIELDS, DATA)).to.deep.equal(DATA_NO_AUTO);
   });
 });
